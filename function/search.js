5df2dbf1ee493da135767d97aad62a284274a9f3
ginwei <lllyyy5400@gmail.com>
2021-08-10 11:50:16 +0800

    add the web3 to the js

--- function/search.js
+++ function/search.js
@@ -7,3 +7,10 @@ function showCompanyResultLineChart() {
   //get the LineChart function
   companyResultLineChart.innerHTML += `<p>${company}</p>`
 }
+
+contract.search(search_resource).call(function(err, result){
+	if (err) {console.log(err);}
+	if (result) {
+		document.getElementById("dataname").innerHTML = "the" + search_resource + 
+		" factory created " + result[0] + " tons of CO2 in " + result[1];
+});
